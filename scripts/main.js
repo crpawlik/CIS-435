718458643f591d2d6a44a3f9cbff39436ffcf0b0
Christian Hanchett <cahanche@umich.edu>
2021-11-18 19:19:41 -0500

    Error handling w/ notes on what needs to be added

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,11 @@ function displayNotepad() {
 
 }
 
+function displayEditableNote() {
 
+}
 
-
+//      Update all messages to show in new HTML element for messages instead of error       //
 
 function addNote() {
     let username = document.querySelector("#username").value;
@@ -29,6 +31,12 @@ function addNote() {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(info)
+    })
+    .then (function(response) {
+        if (response.status == "200")
+            document.querySelector("#error").innerHTML = "Your note has been added!";
+        else 
+            document.querySelector("#error").innerHTML = "There was an issue trying to add your note";
     });
 
 }
@@ -45,12 +53,16 @@ function viewNote() {
         method: 'GET'
     })
     .then (response => response.json())
-    .then (data => obj = data)
-    .then (() => document.querySelector("#error").innerHTML = obj.text)
+    .then (data => {
+        document.querySelector("#error").innerHTML = data.text;
+    });
+
+
+
 
 }
 
-function editNote() {
+function editNote() {   //Link this to a save changes button
     let username = document.querySelector("#username").value;
     let note = document.querySelector("#textpad").value;    // Query text box edited note is in
 
@@ -73,6 +85,12 @@ function editNote() {
         method: 'PUT',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(info)
+    })
+    .then (function(response) {
+        if (response.status == "200")
+            document.querySelector("#error").innerHTML = "Note updated!";
+        else 
+            document.querySelector("#error").innerHTML = "There was an issue trying to update your note";
     });
 }
 
@@ -86,20 +104,28 @@ function deleteNote() {
 
     fetch(`http://localhost:8000/${username}`, {
         method: 'DELETE'
+    })
+    .then (function(response) {
+        if (response.status == "200")
+            document.querySelector("#error").innerHTML = "Your note has been removed";
+        else 
+            document.querySelector("#error").innerHTML = "There was an issue trying to delete your note";
     });
 }
 
 
 function start() {
-    let newButton = document.querySelector("#newNote");
+    let newNoteButton = document.querySelector("#newNote");
     let viewButton = document.querySelector("#viewNote");
-    let editButton = document.querySelector("#editNote");
+    let editButton = document.querySelector("#editNote"); // Change to show note in an editable text box
+    //let saveChangesButton = document.querySelector("updateNote"); Link to save changes button
     let deleteButton = document.querySelector("#removeNote");
     let noteButton = document.querySelector("#addNote");
 
-    newButton.addEventListener("click", displayNotepad);
+    newNoteButton.addEventListener("click", displayNotepad);
     viewButton.addEventListener("click", viewNote);
-    editButton.addEventListener("click", editNote);
+    editButton.addEventListener("click", editNote); // Change to displayEditableNote function
+    //saveChangesButton.addEventListener("click", editNote);
     deleteButton.addEventListener("click", deleteNote);
     noteButton.addEventListener("click", addNote)
 }
