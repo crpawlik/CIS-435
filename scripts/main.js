074e2f75a0031590454032e9da2e98928470f298
Christian Hanchett <cahanche@umich.edu>
2021-11-23 10:33:18 -0500

    Small adjustments to html and JS functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,34 @@
-function displayNotepad() {
+let storedUsername;
 
+function displayNotepad() {
+    let username = document.querySelector("#username").value;
+    document.querySelector('#textpad');
+
+    if (username == '') {
+        document.querySelector("#error").innerHTML = "Please enter a username";
+        document.querySelector("#error").style.display = "block";
+        return;
+    }
+
+    document.querySelector('#textpad').style.display = "block";
+    document.querySelector('#addNote').style.display = "block";
+    document.querySelector("#error").style.display = "none";
 }
 
 function displayEditableNote() {
+    let username = document.querySelector("#username").value;
+    document.querySelector('#textpad').innerHTML = document.querySelector('#note').innerHTML;
+    document.querySelector('#textpad').style.display = "block";
 
+    if (username == '') {
+        document.querySelector("#error").innerHTML = "Please enter a username";
+        document.querySelector("#error").style.display = "block";
+        return;
+    }
+
+    document.querySelector('#textpad').style.display = "block";
+    document.querySelector('#updateNote').style.display = "block";
+    document.querySelector("#error").style.display = "none";
 }
 
 //      Update all messages to show in new HTML element for messages instead of error       //
@@ -15,10 +40,12 @@ function addNote() {
 
     if (username == '') {
         document.querySelector("#error").innerHTML = "Please enter a username";
+        document.querySelector("#error").style.display = "block";
         return;
     }
     else if (note == '') {
         document.querySelector("#error").innerHTML = " Please enter a note";
+        document.querySelector("#error").style.display = "block";
         return;
     }
 
@@ -33,11 +60,18 @@ function addNote() {
         body: JSON.stringify(info)
     })
     .then (function(response) {
-        if (response.status == "200")
-            document.querySelector("#error").innerHTML = "Your note has been added!";
-        else 
+        if (response.status == "200") {
+            document.querySelector("#display").innerHTML = "Your note has been added!";
+            document.querySelector("#display").style.display = "block";
+            document.querySelector("#error").style.display = "none";
+        }
+        else { 
             document.querySelector("#error").innerHTML = "There was an issue trying to add your note";
+            document.querySelector("#error").style.display = "block";
+            document.querySelector("#display").style.display = "none";
+        }
     });
+    document.querySelector("#error").style.display = "none";
 
 }
 
@@ -46,6 +80,7 @@ function viewNote() {
 
     if (username == '') {
         document.querySelector("#error").innerHTML = "Please enter a username";
+        document.querySelector("#error").style.display = "block";
         return;
     }
 
@@ -54,11 +89,14 @@ function viewNote() {
     })
     .then (response => response.json())
     .then (data => {
-        document.querySelector("#error").innerHTML = data.text;
+        document.querySelector("#note").innerHTML = data.text;
     });
 
-
-
+    storedUsername = username;
+    
+    document.querySelector("#note").style.display = "block";
+    document.querySelector("#editNote").style.display = "block";
+    document.querySelector("#removeNote").style.display = "block";
 
 }
 
@@ -69,10 +107,12 @@ function editNote() {   //Link this to a save changes button
 
     if (username == '') {
         document.querySelector("#error").innerHTML = "Please enter a username";
+        document.querySelector("#error").style.display = "block";
         return;
     }
     else if (note == '') { 
         document.querySelector("#error").innerHTML = " Please enter a note";    // Change this for edited note textbox
+        document.querySelector("#error").style.display = "block";
         return;
     }
 
@@ -87,10 +127,16 @@ function editNote() {   //Link this to a save changes button
         body: JSON.stringify(info)
     })
     .then (function(response) {
-        if (response.status == "200")
-            document.querySelector("#error").innerHTML = "Note updated!";
-        else 
+        if (response.status == "200") {
+            document.querySelector("#display").innerHTML = "Note updated!";
+            document.querySelector("#display").style.display = "block";
+            document.querySelector("#error").style.display = "none";
+        }
+        else {
             document.querySelector("#error").innerHTML = "There was an issue trying to update your note";
+            document.querySelector("#error").style.display = "block";
+            document.querySelector("#display").style.display = "none";
+        }
     });
 }
 
@@ -99,6 +145,7 @@ function deleteNote() {
 
     if (username == '') {
         document.querySelector("#error").innerHTML = "Please enter a username";
+        document.querySelector("#error").style.display = "block";
         return;
     }  
 
@@ -106,10 +153,16 @@ function deleteNote() {
         method: 'DELETE'
     })
     .then (function(response) {
-        if (response.status == "200")
-            document.querySelector("#error").innerHTML = "Your note has been removed";
-        else 
+        if (response.status == "200") {
+            document.querySelector("#display").innerHTML = "Your note has been removed";
+            document.querySelector("#display").style.display = "block";
+            document.querySelector("#error").style.display = "none";
+        }
+        else { 
             document.querySelector("#error").innerHTML = "There was an issue trying to delete your note";
+            document.querySelector("#error").style.display = "block";
+            document.querySelector("#display").style.display = "none";
+        }
     });
 }
 
@@ -118,16 +171,17 @@ function start() {
     let newNoteButton = document.querySelector("#newNote");
     let viewButton = document.querySelector("#viewNote");
     let editButton = document.querySelector("#editNote"); // Change to show note in an editable text box
-    //let saveChangesButton = document.querySelector("updateNote"); Link to save changes button
+    let saveChangesButton = document.querySelector("#updateNote"); 
     let deleteButton = document.querySelector("#removeNote");
     let noteButton = document.querySelector("#addNote");
 
     newNoteButton.addEventListener("click", displayNotepad);
     viewButton.addEventListener("click", viewNote);
-    editButton.addEventListener("click", editNote); // Change to displayEditableNote function
-    //saveChangesButton.addEventListener("click", editNote);
+    editButton.addEventListener("click", displayEditableNote); // Change to displayEditableNote function
+    saveChangesButton.addEventListener("click", editNote);
     deleteButton.addEventListener("click", deleteNote);
-    noteButton.addEventListener("click", addNote)
+    noteButton.addEventListener("click", addNote);
+
 }
 
 window.addEventListener("load", start);
